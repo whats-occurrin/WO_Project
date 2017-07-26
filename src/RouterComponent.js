9405ab041c097e30502c048412c6af07b48f4273
Rule1001 <hello@rule1001.com>
2017-07-26 15:44:31 +0100

    refactored router component

--- src/RouterComponent.js
+++ src/RouterComponent.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import Settings from './components/Settings';
 import EventList from './components/EventList';
 import AppHeader from './components/common/AppHeader';
 import NewEvent from './components/NewEvent';
+import EventListPage from './components/Pages/EventListPage';
+import EventCard from './components/Pages/EventCard';
 
 class RouterComponent extends Component {
 
@@ -12,14 +14,11 @@ class RouterComponent extends Component {
       <Router sceneStyle={{ paddingTop: 65 }}>
 
         <Scene key="root">
-           <Scene initial key="eventList" component={EventList} title="EventList" /> 
-                     <Scene key="appHeader" component={AppHeader} title="AppHeader" />
-
-        <Scene key="newEvent" component={NewEvent} title="NewEvent"/>
-
+          <Scene initial key="eventListPage" component={EventListPage} title="EventListPage" />
           <Scene key="settings" component={Settings} title="Settings" />
-
-
+          <Scene key="appHeader" component={AppHeader} title="AppHeader" />
+          <Scene key="displayCard" component={EventCard} title="Card" />
+          <Scene key="newEvent" component={NewEvent} title="NewEvent" />
         </Scene>
 
       </Router>
