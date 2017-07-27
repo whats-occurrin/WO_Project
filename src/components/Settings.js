b1399dbb686c480fef9f4a5f832a892eb865f336
Gemma <gemmawvoakes@gmail.com>
2017-07-27 20:28:33 +0100

    Settings page layout improvements.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,38 +1,50 @@
 import React, { Component } from 'react';
-import { Text, Slider } from 'react-native';
-//import Button from './common';
+import { Text, View, Slider } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import AppHeader from './common/AppHeader';
+import { Button } from 'native-base';
 
-import { Container, Button } from 'native-base';
 
 class Settings extends Component {
     render() {
         return (
-            <Container >
-                <AppHeader headerText={'What\'s Occurring?'}/>
-                <Text>Settings!</Text>
-                <Text>{'I\'m interested in events within a '}</Text>
-                <Slider></Slider>
-                <Text>mile radius</Text>
-                <Text>Please select the event types you are interested in...</Text>
+            <View >
+                <AppHeader headerText={'What\'s Occurring?'} />
+                <Text style={styles.textStyle}>{'I\'m interested in events within a '}</Text>
+                <Slider minimumValue={0} maximumValue={20} step={2} value={2} thumbTintColor={'#fc7401'}></Slider>
+                <Text style={styles.textStyle}>mile radius</Text>
+                <Text style={styles.textStyle}>Please select the event types you are interested in...</Text>
 
-                <Button ><Text>Restaurants</Text></Button><Button ><Text>Pubs and bars</Text></Button>
+                <View style={{height: 60, display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginTop: 15}}>
+                    <Button rounded info bordered><Text>Restaurants</Text></Button><Button rounded info bordered><Text>Pubs and bars</Text></Button>
+                </View>
 
-                <Button ><Text>Theatre</Text></Button><Button ><Text>Sports</Text></Button>
+                <View style={{height: 70, display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginTop: 15}}>
+                    <Button rounded info bordered><Text>Theatre</Text></Button><Button rounded info bordered><Text>Sports</Text></Button>
+                </View>
+                
+                <View style={{height: 60, display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 5}}>
+                    <Button rounded success onPress={() => Actions.eventListPage()}>
+                        <Text >Update my preferences</Text>
+                    </Button>
+                </View>
 
-                <Button onPress={() => Actions.eventListPage()}>
-                    <Text>Update my preferences</Text>
-                </Button>
-            
-                <Button onPress={() => Actions.newEvent()}>
-                    <Text>Add an event</Text>
-                </Button>
-
-
-            </Container>
+                <View style={{height: 50, display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 5, paddingTop: 20}}>
+                    <Button rounded warning onPress={() => Actions.newEvent()}>
+                        <Text>Add an event</Text>
+                    </Button>
+                </View>
+            </View>
         );
     }
 }
+
+const styles = {
+    textStyle: {
+        fontSize: 18,
+        color: '#000000',
+        padding: 10
+    }
+};
 
 export default Settings;
