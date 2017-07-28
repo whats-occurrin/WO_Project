4196e3c903c0618920a53078af403fc36e88277c
Gemma <gemmawvoakes@gmail.com>
2017-07-28 09:56:42 +0100

    OnPress function added to change button colour

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,6 +6,15 @@ import { Button } from 'native-base';
 
 
 class Settings extends Component {
+
+    state = {
+        restaurants: false,
+        pubs: false,
+        theatre: false,
+        sports: false
+
+    }
+
     render() {
         return (
             <View >
@@ -15,21 +24,53 @@ class Settings extends Component {
                 <Text style={styles.textStyle}>mile radius</Text>
                 <Text style={styles.textStyle}>Please select the event types you are interested in...</Text>
 
-                <View style={{height: 60, display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginTop: 15}}>
-                    <Button rounded info bordered><Text>Restaurants</Text></Button><Button rounded info bordered><Text>Pubs and bars</Text></Button>
+                <View style={{ height: 60, display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginTop: 15 }}>
+                    <Button
+                        rounded
+                        info={this.state.restaurants}
+                        bordered={!this.state.restaurants}
+                        onPress={() => this.setState({ restaurants: !this.state.restaurants })}
+                    >
+                        <Text>Restaurants</Text>
+                    </Button>
+
+                    <Button
+                        rounded
+                        info={this.state.pubs}
+                        bordered={!this.state.pubs}
+                        onPress={() => this.setState({ pubs: !this.state.pubs })}
+                    >
+                        <Text>Pubs and bars</Text>
+                    </Button>
                 </View>
 
-                <View style={{height: 70, display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginTop: 15}}>
-                    <Button rounded info bordered><Text>Theatre</Text></Button><Button rounded info bordered><Text>Sports</Text></Button>
+                <View style={{ height: 70, display: 'flex', flexDirection: 'row', justifyContent: 'space-around', marginTop: 15 }}>
+                    <Button
+                        rounded
+                        info={this.state.theatre}
+                        bordered={!this.state.theatre}
+                        onPress={() => this.setState({ theatre: !this.state.theatre })}
+                    >
+                        <Text>Theatre</Text>
+                    </Button>
+
+                    <Button
+                        rounded
+                        info={this.state.sports}
+                        bordered={!this.state.sports}
+                        onPress={() => this.setState({ sports: !this.state.sports })}
+                    >
+                        <Text>Sports</Text>
+                    </Button>
                 </View>
-                
-                <View style={{height: 60, display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 5}}>
+
+                <View style={{ height: 60, display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 5 }}>
                     <Button rounded success onPress={() => Actions.eventListPage()}>
                         <Text >Update my preferences</Text>
                     </Button>
                 </View>
 
-                <View style={{height: 50, display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 5, paddingTop: 20}}>
+                <View style={{ height: 50, display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 5, paddingTop: 20 }}>
                     <Button rounded warning onPress={() => Actions.newEvent()}>
                         <Text>Add an event</Text>
                     </Button>
@@ -38,6 +79,8 @@ class Settings extends Component {
         );
     }
 }
+
+
 
 const styles = {
     textStyle: {
